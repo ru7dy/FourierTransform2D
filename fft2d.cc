46ceb44c075f625ce36e53060c9afdcb8227a1ac
ru7dy <rudymelody@gmail.com>
2015-09-03 19:45:07 -0400

    2d transform done

--- fft2d.cc
+++ fft2d.cc
@@ -17,7 +17,9 @@
 
 using namespace std;
 
-void centralize(int numtasks, int rank, int total_len, int local_num, Complex* after1d_local, Complex* after1d);
+void distribute(int numtasks, int rank, int total_len, int local_num, Complex* matrix);
+void transpose(Complex* in, Complex* out, int w, int h);
+void centralize(int numtasks, int rank, int total_len, int local_num, Complex* matrix);
 void Transform1D(Complex* h, int w, Complex* H);
 
 void Transform2D(const char* inputFN) 
@@ -56,7 +58,7 @@ void Transform2D(const char* inputFN)
 
   
   Complex* after1d = new Complex[total_len];
-  Complex* after1d_local = new Complex[local_num];
+  // Complex* after1d_local = new Complex[local_num];
 
   // since all nodes read the input, do the first 1d fft locally
   // Complex* after1d_local = new Complex[local_num];
@@ -66,29 +68,11 @@ void Transform2D(const char* inputFN)
   for(int i = 0; i < height / numtasks; i++) {
     global_offset = width * (startRow + i);
     local_offset = width * i;
-    Transform1D(input + global_offset, width, after1d_local + local_offset);
+    Transform1D(input + global_offset, width, after1d + local_offset);
   }
 
   // slaves send data after 1d fft back to master
-  centralize(numtasks, rank, total_len, local_num, after1d_local, after1d);
-
-  // master node gathers data, real part
-  // for(int i = 0; i < local_num; i++) {
-  //   recvbuf[i] = after1d_local[i].real;
-  // }
-  // MPI_Gatherv(recvbuf, local_num, MPI_DOUBLE, sendbuf, sendcount, displ, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  // for(int i = 0; i < total_len; i++) {
-  //   after1d[i].real = sendbuf[i];
-  // }
-
-  // // master node gathers data, imag part
-  // for(int i = 0; i < local_num; i++) {
-  //   recvbuf[i] = after1d_local[i].imag;
-  // }
-  // MPI_Gatherv(recvbuf, local_num, MPI_DOUBLE, sendbuf, sendcount, displ, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  // for(int i = 0; i < total_len; i++) {
-  //   after1d[i].imag = sendbuf[i];
-  // }
+  centralize(numtasks, rank, total_len, local_num, after1d);
 
   if(rank == 0)
   {
@@ -96,19 +80,75 @@ void Transform2D(const char* inputFN)
     image.SaveImageData("MyAfter1d.txt", after1d, width, height);
   }
 
-  delete[] after1d;
-  delete[] after1d_local;
+  // transpose the matrix on master
+  Complex* transposed1d = new Complex[total_len];
+  if(rank == 0) {
+    transpose(after1d, transposed1d, width, height);
+  }
+
+  // master sends the transposed data to slaves
+  distribute(numtasks, rank, total_len, local_num, transposed1d);
+
+  // slaves do 1d fft, which results in 2d fft
+  Complex* after2d = new Complex[total_len];
+  for(int i = 0; i < height / numtasks; i++) {
+    global_offset = width * (startRow + i);
+    local_offset = width * i;
+    Transform1D(transposed1d + global_offset, width, after2d + local_offset);
+  }
+
+  // slaves send 2d fft results back to master
+  centralize(numtasks, rank, total_len, local_num, after2d);
+
+  // master transposes 2d fft result and save
+  Complex* transposed2d = new Complex[total_len];
+  if(rank == 0) {
+    transpose(after2d, transposed2d, width, height);
+  }
+
+  if(rank == 0)
+  {
+    cout<<"Generating Image File MyAfter2d.txt"<<endl;
+    image.SaveImageData("MyAfter2d.txt", transposed2d, width, height);
+  }
+
+
+
+  // delete[] after1d;
+  // delete[] after1d_local;
 }
 
-void centralize(int numtasks, int rank, int total_len, int local_num, Complex* after1d_local, Complex* after1d) {
+void centralize(int numtasks, int rank, int total_len, int local_num, Complex* matrix) {
   if(rank != 0) { // slaves send back to master
     MPI_Request request;
-    MPI_Isend(after1d_local, local_num * sizeof(Complex), MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
+    MPI_Isend(matrix, local_num * sizeof(Complex), MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
   }
   else { // master receives all the data from slaves
-    MPI_Status status;
-    MPI_Recv(after1d + i * local_num, local_num * sizeof(Complex), MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
+    for(int i = 1; i < numtasks; i++) {
+      MPI_Status status;
+      MPI_Recv(matrix + i * local_num, local_num * sizeof(Complex), MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
+    }
   }
+}
+
+void distribute(int numtasks, int rank, int total_len, int local_num, Complex* matrix) {
+  if(rank != 0) {
+    MPI_Status status;
+    MPI_Recv(matrix, total_len * sizeof(Complex), MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+  }
+  else {
+    for(int i = 1; i < numtasks; i++) {
+      MPI_Request request;
+      MPI_Isend(matrix, total_len * sizeof(Complex), MPI_CHAR, i, 0, MPI_COMM_WORLD, &request);
+    }
+  }
+}
+
+void transpose(Complex* in, Complex* out, int w, int h) {
+  int p = 0;
+  for(int i = 0; i < h; i++) 
+    for(int j = 0; j < w; j++) 
+      out[p++] = in[i + j * w];
 }
 
 void Transform1D(Complex* h, int w, Complex* H)
